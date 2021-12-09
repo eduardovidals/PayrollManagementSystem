1ff6f9a6bbfcd9d2e4323e6b525f0731efa869d2
Christopher Torres <christopherjtorres01@gmail.com>
2021-12-09 17:30:10 -0500

    Testing

--- src/main/java/computerscience/cs306/payrollmanagementsystem/PayrollManagementSystemApplication.java
+++ src/main/java/computerscience/cs306/payrollmanagementsystem/PayrollManagementSystemApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//This is a test
+// This is a test
+// Unicone
 public class PayrollManagementSystemApplication {
     public static void main(String[] args) {
         SpringApplication.run(PayrollManagementSystemApplication.class, args);
