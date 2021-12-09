09688fb3769a17b2f5c874eb1d4544ce8bdce3e1
Christopher Torres <christopherjtorres01@gmail.com>
2021-12-09 18:06:03 -0500

    final commit

--- src/main/java/computerscience/cs306/payrollmanagementsystem/Employee.java
+++ src/main/java/computerscience/cs306/payrollmanagementsystem/Employee.java
@@ -6,7 +6,6 @@ import javax.persistence.GeneratedValue;
 import java.util.Objects;
 
 @Entity
-// Unicorn class
 public class Employee {
     private @Id @GeneratedValue Long id;
     private String firstName;
