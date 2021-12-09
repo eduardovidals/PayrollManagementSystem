d8f32cfe0c923aa4e0a7284fe70d6154f129cb4c
Christopher Torres <christopherjtorres01@gmail.com>
2021-12-09 17:54:40 -0500

    Test

--- src/main/java/computerscience/cs306/payrollmanagementsystem/PayrollManagementSystemApplication.java
+++ src/main/java/computerscience/cs306/payrollmanagementsystem/PayrollManagementSystemApplication.java
@@ -2,7 +2,7 @@ package computerscience.cs306.payrollmanagementsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// Test
 @SpringBootApplication
 public class PayrollManagementSystemApplication {
     public static void main(String[] args) {
