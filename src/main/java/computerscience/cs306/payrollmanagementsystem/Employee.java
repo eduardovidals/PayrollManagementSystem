add738f7244b7cf1e58b8badc8271e749eaf682e
Christopher Torres <christopherjtorres01@gmail.com>
2021-12-09 17:37:50 -0500

    Deleted comments

--- src/main/java/computerscience/cs306/payrollmanagementsystem/Employee.java
+++ src/main/java/computerscience/cs306/payrollmanagementsystem/Employee.java
@@ -6,9 +6,6 @@ import javax.persistence.GeneratedValue;
 import java.util.Objects;
 
 @Entity
-// unicorn test test
-// this is an employee
-// test test test
 public class Employee {
     private @Id @GeneratedValue Long id;
     private String firstName;
